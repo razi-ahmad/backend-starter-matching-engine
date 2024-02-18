8834d68e5f49fa141017f25d33caa1fe28301e36
razi.ahmad <razi.ahmad@justeattakeaway.com>
2024-02-19 00:18:26 +0100

    Added integration tests

--- src/test/java/io/bux/matchingengine/web/TradingControllerIntegrationTest.java
+++ src/test/java/io/bux/matchingengine/web/TradingControllerIntegrationTest.java
@@ -3,6 +3,7 @@ package io.bux.matchingengine.web;
 import io.bux.matchingengine.dto.OrderRequest;
 import io.bux.matchingengine.dto.OrderResponse;
 import io.bux.matchingengine.enums.Direction;
+import io.bux.matchingengine.util.MessageConstant;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestClientException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -24,6 +26,17 @@ public class TradingControllerIntegrationTest {
     public void test_when_no_order_exist() {
         String content = this.restTemplate.getForObject(BASE_URL + "/" + Long.MAX_VALUE, String.class);
         Assertions.assertEquals("Order not found", content);
+    }
+
+    @Test(expected = RestClientException.class)
+    public void test_place_order_when_request_is_null() {
+        this.restTemplate.postForEntity(BASE_URL, null, OrderResponse.class);
+    }
+
+    @Test
+    public void test_place_order_when_request_is_empty() {
+        ResponseEntity<String> content=this.restTemplate.postForEntity(BASE_URL, new OrderRequest(null, null, null, null), String.class);
+        Assertions.assertEquals(MessageConstant.EMPTY_ORDER_REQUEST_ASSET_ERROR,content.getBody());
     }
 
     @Test
@@ -47,14 +60,14 @@ public class TradingControllerIntegrationTest {
         OrderResponse orderResponse = this.restTemplate.getForObject(BASE_URL + "/3", OrderResponse.class);
         Assertions.assertNotNull(orderResponse);
         Assertions.assertNotNull(orderResponse.trades());
-        Assertions.assertEquals(3,orderResponse.id());
-        Assertions.assertEquals(43251.00,orderResponse.price());
-        Assertions.assertEquals(0.65,orderResponse.amount());
-        Assertions.assertEquals(Direction.BUY,orderResponse.direction());
-        Assertions.assertEquals(0.00,orderResponse.pendingAmount());
-        Assertions.assertEquals(1,orderResponse.trades().size());
-        Assertions.assertEquals(0,orderResponse.trades().get(0).orderId());
-        Assertions.assertEquals(0.65,orderResponse.trades().get(0).amount());
-        Assertions.assertEquals(43251.00,orderResponse.trades().get(0).price());
+        Assertions.assertEquals(3, orderResponse.id());
+        Assertions.assertEquals(43251.00, orderResponse.price());
+        Assertions.assertEquals(0.65, orderResponse.amount());
+        Assertions.assertEquals(Direction.BUY, orderResponse.direction());
+        Assertions.assertEquals(0.00, orderResponse.pendingAmount());
+        Assertions.assertEquals(1, orderResponse.trades().size());
+        Assertions.assertEquals(0, orderResponse.trades().get(0).orderId());
+        Assertions.assertEquals(0.65, orderResponse.trades().get(0).amount());
+        Assertions.assertEquals(43251.00, orderResponse.trades().get(0).price());
     }
 }
